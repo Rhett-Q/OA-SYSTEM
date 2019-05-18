b2e7a9a7c9d65ac2b654f744ed255087a24d64a1
1239793368 <50760769+1239793368@users.noreply.github.com>
2019-05-18 20:15:01 +0800

    test
    项目完善ing.......

---
+++ cgboa/src/main/java/com/cgboa/sys/controller/PlanController.java
@@ -0,0 +1,15 @@
+package com.cgboa.sys.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/")
+@Controller
+public class PlanController {
+
+	@RequestMapping("planview")
+	public String doPlanView(){
+		return "plan/planview";
+		
+	}
+}
