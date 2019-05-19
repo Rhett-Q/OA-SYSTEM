67ce534f6971ea6e2aeef7b3424ff1fa71ba82ea
Rhett <1716814539@qq.com>
2019-05-19 14:31:33 +0800

    init

--- cgboa/src/main/java/com/cgboa/sys/dao/SysUserDao.java
+++
@@ -1,9 +0,0 @@
-package com.cgboa.sys.dao;
-
-import com.cgboa.sys.entity.SysUser;
-
-public interface SysUserDao {
-	
-	SysUser findObjectByUserId(Integer userId);
-	
-}
--- cgboa/src/main/java/com/cgboa/sys/service/SysUserService.java
+++
@@ -1,9 +0,0 @@
-package com.cgboa.sys.service;
-
-import com.cgboa.sys.entity.SysUser;
-
-public interface SysUserService {
-	
-	SysUser findObjectByUserId(Integer userId);
-	
-}
